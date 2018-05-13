6a10eb54934e8934db34364a0a0403de6a2687cc
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-05-13 16:49:25 +0900

    Set Carthage

---
+++ ios-rx-timer-sample/ios-rx-timer-sampleTests/ios_rx_timer_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_rx_timer_sampleTests.swift
+//  ios-rx-timer-sampleTests
+//
+//  Created by ogasawara_dev on 2018/05/13.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import XCTest
+@testable import ios_rx_timer_sample
+
+class ios_rx_timer_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
